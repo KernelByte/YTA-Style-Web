b7640ff37d1edc849030c351367535c34459ff07
Yoniher Melendez <yonihermelendez@gmail.com>
2024-04-23 16:23:27 -0500

    Event binding en algular para ejecucion de eventos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,11 +9,4 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  person = {
-    name: "Orlando Escobar",
-    age: 27,
-    country: "Colombia",
-    pasaport: "https://img.freepik.com/vector-gratis/avatar-personaje-empresario-aislado_24877-60111.jpg?t=st=1713899982~exp=1713900582~hmac=c294841c72cd8241c34fd930797fd939a7b34b19c140a7e8fa257fe09b24bacd"
-  }
-
 }
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -11,4 +11,24 @@ import { Component } from '@angular/core';
 export class LabsComponent {
   welcome = 'YTA Style';
   tasks = ['jugar','Estudiar','Trabajar','Leer']
+  person = {
+    name: "Orlando Escobar",
+    age: 27,
+    country: "Colombia",
+    pasaport: "https://img.freepik.com/vector-gratis/avatar-personaje-empresario-aislado_24877-60111.jpg?t=st=1713899982~exp=1713900582~hmac=c294841c72cd8241c34fd930797fd939a7b34b19c140a7e8fa257fe09b24bacd"
+  }
+
+  clickHandler(){
+    alert("¿Hola como estas?")
+  }
+
+  changehandler(event: Event){
+    console.log(event)
+  }
+
+  changehandlerkey(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value)
+  }
+
 }

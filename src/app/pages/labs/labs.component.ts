7350b2918849031ac931806375cbee868a770c51
Yoniher Melendez <yonihermelendez@gmail.com>
2024-04-23 14:21:27 -0500

    property binding dentro de angular

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,12 +1,14 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css'
 })
 export class LabsComponent {
-
+  welcome = 'YTA Style';
+  tasks = ['jugar','Estudiar','Trabajar','Leer']
 }

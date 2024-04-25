21855ae415d5d8ba683531b01c8b65164f732991
Yoniher Melendez <yonihermelendez@gmail.com>
2024-04-25 15:43:18 -0500

    Se reinicia a un nuevo proyecto de e-comerce

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NgFor],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
 export class AppComponent {
-  title = 'YTA Style';
-  tasks = ['jugar','Estudiar','Trabajar','Leer']
+  title = 'ytastyle';
 }

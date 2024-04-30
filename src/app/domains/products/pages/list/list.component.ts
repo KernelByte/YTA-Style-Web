d8f134ba12985f9d4fa42ca35dcf5d3aaa445a69
Yoniher Melendez <yonihermelendez@gmail.com>
2024-04-30 14:59:53 -0500

    Conexion de servicio con API Rest de productos fake

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,9 +1,9 @@
 import { Component, inject, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
 import { Product } from './../../../shared/models/product.model';
-import { Title } from '@angular/platform-browser';
 import { HeaderComponent } from './../../../shared/components/header/header.component';
 import { CartService } from './../../../shared/services/cart.service';
+import { ProductService } from '../../../shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -15,69 +15,22 @@ import { CartService } from './../../../shared/services/cart.service';
 export class ListComponent {
 
   produtSignal = signal<Product[]>([]);
+
   cart = signal<Product[]>([]);
 
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-  constructor(){
-    const initProducts: Product[] = [{
-      id: Date.now(),
-      title: 'Producto 1',
-      price: 2000,
-      image: 'https://picsum.photos/480/480?r=23',
-      createAt: new Date().toISOString()
-    },
-    {
-      id: Date.now(),
-      title: 'Producto 2',
-      price: 3000,
-      image: 'https://picsum.photos/480/480?r=24',
-      createAt: new Date().toISOString()
-    },
-    {
-      id: Date.now(),
-      title: 'Producto 3',
-      price: 3000,
-      image: 'https://picsum.photos/480/480?r=25',
-      createAt: new Date().toISOString()
-    },
-    {
-      id: Date.now(),
-      title: 'Producto 4',
-      price: 3000,
-      image: 'https://picsum.photos/480/480?r=26',
-      createAt: new Date().toISOString()
-    },
-    {
-      id: Date.now(),
-      title: 'Producto 5',
-      price: 3000,
-      image: 'https://picsum.photos/480/480?r=27',
-      createAt: new Date().toISOString()
-    },
-    {
-      id: Date.now(),
-      title: 'Producto 6',
-      price: 3000,
-      image: 'https://picsum.photos/480/480?r=28',
-      createAt: new Date().toISOString()
-    },
-    {
-      id: Date.now(),
-      title: 'Producto 7',
-      price: 3000,
-      image: 'https://picsum.photos/480/480?r=29',
-      createAt: new Date().toISOString()
-    },
-    {
-      id: Date.now(),
-      title: 'Producto 8',
-      price: 3000,
-      image: 'https://picsum.photos/480/480?r=30',
-      createAt: new Date().toISOString()
-    }];
-
-    this.produtSignal.set(initProducts);
+  ngOnInit() {
+    this.productService.getProducts()
+      .subscribe({
+        next: (products) => {
+          this.produtSignal.set(products);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });    
   }
 
   addToCart(newProduct: Product)
---
+++ src/app/domains/shared/services/product.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Product } from '../models/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private http = inject(HttpClient);
+
+  constructor() { }
+
+  // Obtener los productos de la API
+  getProducts(){
+    return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products');
+  }
+}

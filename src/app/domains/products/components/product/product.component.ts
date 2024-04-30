b6e8edefa709fae4fc3ece9d74f905a0ff9a31dd
Yoniher Melendez <yonihermelendez@gmail.com>
2024-04-30 15:35:41 -0500

    Creacion de un pipe personalizado para voltear texto

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../../shared/models/product.model';
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import {ReversePipe} from '@shared/pipes/reverse.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CurrencyPipe, DatePipe, UpperCasePipe],
+  imports: [CurrencyPipe, DatePipe, UpperCasePipe,ReversePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })

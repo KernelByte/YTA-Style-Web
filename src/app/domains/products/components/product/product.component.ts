f941dc5f25107e173c91a27cdf28374c798623cc
Yoniher Melendez <yonihermelendez@gmail.com>
2024-05-02 16:26:00 -0500

    Detalle de cada producto y uso de routerLink

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -3,11 +3,12 @@ import { Product } from '../../../shared/models/product.model';
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import {ReversePipe} from '@shared/pipes/reverse.pipe';
 import {TimeAgoPipe} from '@shared/pipes/time-ago.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [CurrencyPipe, DatePipe, UpperCasePipe,ReversePipe,TimeAgoPipe],
+  imports: [CurrencyPipe, DatePipe, UpperCasePipe,ReversePipe,TimeAgoPipe,RouterLink],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
